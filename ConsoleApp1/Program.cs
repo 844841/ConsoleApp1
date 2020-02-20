b00d933fc6661903331b1ea553aefe36ff3d5635
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:36:02 +0530

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            float a, b;
+            double c;
+            Console.WriteLine("enter first number");
+            a =float.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            b =float.Parse(Console.ReadLine());
+            c = a * b;
+            Console.WriteLine("product of "+a+" and "+b+" is"  +c);
+            
+
+
+
+
+        }
+    }
+}
